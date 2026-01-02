64f2c066503c79bd7a3b3d887d4e5099e0001f84
Casmo <385764+Casmo@users.noreply.github.com>
2026-01-02 15:24:22 +0000

    fix: styling

--- database/factories/RedirectFactory.php
+++ database/factories/RedirectFactory.php
@@ -20,7 +20,7 @@ class RedirectFactory extends Factory
             'source' => '/' . $this->faker->unique()->slug(),
             'destination' => '/' . $this->faker->slug(),
             'code' => 301,
-            'hits' => 0
+            'hits' => 0,
         ];
     }
 }
