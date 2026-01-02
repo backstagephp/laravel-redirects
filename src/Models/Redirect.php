7e433ba2f0bcb9650443f6be49c8cdcf2341805e
Mathieu <mathieu@fellicht.nl>
2026-01-02 16:23:51 +0100

    Correct file paths

--- src/Models/Redirect.php
+++ src/Models/Redirect.php
@@ -2,6 +2,7 @@
 
 namespace Backstage\Redirects\Laravel\Models;
 
+use Backstage\Redirects\Laravel\Database\Factories\RedirectFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -21,6 +22,11 @@ class Redirect extends Model
         'destination',
         'code',
     ];
+
+    protected static function newFactory()
+    {
+        return RedirectFactory::new();
+    }
 
     public function redirect(Request $request): ?RedirectResponse
     {
