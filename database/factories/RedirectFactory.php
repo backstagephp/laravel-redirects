50f45e029caea9ddc0ca6737aea866227cd18bab
Backstage <hello@backstagephp.com>
2026-01-13 14:48:24 +0000

    Add user management

---
+++ database/factories/RedirectFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Backstage\Redirects\Laravel\Database\Factories;
+
+use Backstage\Redirects\Laravel\Models\Redirect;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RedirectFactory extends Factory
+{
+    protected $model = Redirect::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'source' => '/' . $this->faker->unique()->slug(),
+            'destination' => '/' . $this->faker->slug(),
+            'code' => 301,
+            'hits' => 0,
+        ];
+    }
+}
