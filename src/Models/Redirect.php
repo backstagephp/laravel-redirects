4a2f0f7132cfff2f8882b3b6e274606b840ff36c
Backstage <hello@backstagephp.com>
2026-01-23 15:20:26 +0000

    Add docs to monorepo split

--- src/Models/Redirect.php
+++ src/Models/Redirect.php
@@ -35,7 +35,7 @@ class Redirect extends Model
         $destination = $this->destination;
 
         if ($request->query()) {
-            $destination .= (str($destination)->contains('?') ? '&' : '?').Arr::query($request->query());
+            $destination .= (str($destination)->contains('?') ? '&' : '?') . Arr::query($request->query());
         }
 
         return redirect($destination, $this->code)
