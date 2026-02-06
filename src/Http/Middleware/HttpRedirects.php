ea1c68bd756283ff1a47c2a1bd4964488a66ac5b
Backstage <hello@backstagephp.com>
2026-02-06 14:44:45 +0000

    Revert middlewares

--- src/Http/Middleware/HttpRedirects.php
+++ src/Http/Middleware/HttpRedirects.php
@@ -13,34 +13,25 @@ class HttpRedirects
 
     public function handleNonPost(Request $request, Closure $next)
     {
-        $currentUrl = str($request->fullUrl())
-            ->replace(['http://', 'https://'], '')
-            ->replace(['www.'], '')
-            ->rtrim('/')
-            ->toString();
+        /**
+         * @var \Backstage\Redirects\Laravel\Models\Redirect|null $checker
+         */
+        $checker = Redirect::all()
+            ->firstWhere(function (Redirect $redirect) use ($request) {
+                return str($request->fullUrl())
+                    ->replace(['http://', 'https://'], '')
+                    ->replace(['www.'], '')
+                    ->contains(
+                        str($redirect->source)
+                            ->replace(['http://', 'https://'], '')
+                            ->replace(['www.'], '')
+                    );
+            });
 
-        $currentPath = str($request->getRequestUri())
-            ->rtrim('/')
-            ->toString();
-
-        // Match relative paths against path only, full URLs against full URL
-        $redirect = Redirect::query()
-            ->where(function ($query) use ($currentUrl, $currentPath) {
-                $query->where(function ($q) use ($currentPath) {
-                    $q->whereRaw("source LIKE '/%'")
-                        ->whereRaw("TRIM(TRAILING '/' FROM source) = ?", [$currentPath]);
-                })
-                    ->orWhere(function ($q) use ($currentUrl) {
-                        $q->whereRaw("source NOT LIKE '/%'")
-                            ->whereRaw("TRIM(TRAILING '/' FROM REPLACE(REPLACE(source, 'https://', ''), 'http://', '')) = ?", [$currentUrl]);
-                    });
-            })
-            ->first();
-
-        if (! $redirect) {
+        if (! $checker) {
             return $next($request);
         }
 
-        return $redirect->redirect($request);
+        return $checker->redirect($request);
     }
 }
